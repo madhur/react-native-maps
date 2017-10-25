7c37793b31c7fb759757021686c76bf546a6dd68
Madhur <ahuja.madhur@gmail.com>
2017-10-25 16:37:19 +0530

    deck screen

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -6,7 +6,7 @@ import {
     FETCH_JOBS
 } from './types';
 
-const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 
 const JOB_QUERY_PARAMS = {
     publisher: '4201738803816157',
@@ -22,13 +22,14 @@ const buildJobsUrl = (zip) => {
     return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = (region) => async (dispatch) => {
+export const fetchJobs = (region, callback) => async (dispatch) => {
     try {
         let zip = await reverseGeocode(region);
         const url = buildJobsUrl(zip);
 
         let { data } = await axios.get(url);
         dispatch({ type: FETCH_JOBS, payload: data });
+        callback();
         console.log(data);
     } catch (e) {
         console.error(e);
--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import Swipe from '../components/Swipe';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
 
 class DeckScreen extends Component {
+
+    renderCard(job) {
+        return (
+            <Card title={job.jobtitle}>
+                <View style={styles.detailWrapper}>
+                    <Text>{job.company}</Text>
+                    <Text>{job.formattedRelativeTime}</Text>
+                </View>
+                <Text>
+                    {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+                </Text>
+            </Card>
+        );
+    }
+
+    renderNoMoreCards() {
+        return (
+            <Card title="No more jobs">
+
+            </Card>
+        )
+    }
 
     render() {
         return (
             <View>
-                <Text>DeckScreen</Text>
+                <Swipe data={this.props.jobs} renderCard={this.renderCard} renderNoMoreCards={this.renderNoMoreCards} />
             </View>
         );
     }
 }
 
-export default DeckScreen;
+const styles = {
+    detailWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 10
+    }
+};
+
+function mapStateToProps({ jobs }) {
+    return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
