ed8519a86538f10d47c48f4bd7b7c46ce3f7d1aa
Madhur <ahuja.madhur@gmail.com>
2017-10-25 18:02:05 +0530

    changes

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -38,12 +38,19 @@ class DeckScreen extends Component {
         );
     }
 
-    renderNoMoreCards() {
+    renderNoMoreCards = () => {
         return (
             <Card title="No more jobs">
+                <Button
+                    title="Back to Map"
+                    large
+                    icon={{ name: 'my-location' }}
+                    backgroundColor="#03a9f4"
+                    onPress={() => this.props.navigation.navigate('map')}
+                ></Button>
 
             </Card>
-        )
+        );
     }
 
     render() {
